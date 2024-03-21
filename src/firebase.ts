949f94ba2d9a99e0e4d687d2a28c9bb8c7ea9c81
unknown <psdkei@naver.com>
2024-03-21 19:01:55 +0900

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,12 +8,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyChCpepxvRTq3vqhFpbgYWbAFhSzW20wCM",
-  authDomain: "twitter-reloaded-96616.firebaseapp.com",
-  projectId: "twitter-reloaded-96616",
-  storageBucket: "twitter-reloaded-96616.appspot.com",
-  messagingSenderId: "646245055726",
-  appId: "1:646245055726:web:454ebb5f82998f92c147ff",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
